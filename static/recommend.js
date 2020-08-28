448733af194da22edb17a74e74015f6b0809d225
Saikot Majumdar <saikot.ece@gmail.com>
2020-08-29 01:36:09 +0530

    update api key

--- static/recommend.js
+++ static/recommend.js
@@ -12,7 +12,7 @@ $(function() {
   source.addEventListener('input', inputHandler);
 
   $('.movie-button').on('click',function(){
-    var my_api_key = 'YOUR_API_KEY';
+    var my_api_key = 'fa121b8b91b642217b94881eca90a47c';
     var title = $('.movie').val();
     if (title=="") {
       $('.results').css('display','none');
@@ -26,7 +26,7 @@ $(function() {
 
 // will be invoked when clicking on the recommended movies
 function recommendcard(e){
-  var my_api_key = 'YOUR_API_KEY';
+  var my_api_key = 'fa121b8b91b642217b94881eca90a47c';
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
